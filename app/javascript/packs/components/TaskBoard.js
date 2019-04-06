3fb05d4387aa3ef291ffbc7062a42ad894643977
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-04-06 16:17:08 +0400

    Dragging (and state change) logic implemented.

--- app/javascript/packs/components/TaskBoard.js
+++ app/javascript/packs/components/TaskBoard.js
@@ -4,92 +4,127 @@ import LaneHeader from './LaneHeader';
 import { fetch } from './Fetch';
 
 export default class TasksBoard extends React.Component {
-    state = {
-      board: {
-        new_task: null,
-        in_development: null,
-        in_qa: null,
-        in_code_review: null,
-        ready_for_release: null,
-        released: null,
-        archived: null,
-      },
-    }
+  state = {
+    board: {
+      new_task: null,
+      in_development: null,
+      in_qa: null,
+      in_code_review: null,
+      ready_for_release: null,
+      released: null,
+      archived: null,
+    },
+  }
 
-    generateLane(id, title) {
-      const tasks = this.state[id];
+  state_events = {
+    new_task: null,
+    in_development: 'start_develop',
+    in_qa: 'start_qa',
+    in_code_review: 'start_review',
+    ready_for_release: 'mark_as_ready',
+    released: 'release',
+    archived: 'archive',
+  }
 
-      return {
-        id,
-        title,
-        total_count: (tasks) ? tasks.meta.total_count : 'None',
-        cards: (tasks) ? tasks.items.map(task => ({
+  generateLane(id, title, state_event) {
+    const tasks = this.state[id];
+
+    return {
+      id,
+      title,
+      state_event,
+      total_count: (tasks) ? tasks.meta.total_count : 'None',
+      cards: (tasks) ? tasks.items.map(task => ({
+        ...task,
+        label: task.state,
+        title: task.name,
+      })) : [],
+    };
+  }
+
+  getStateEventName = stateId => this.state_events[stateId];
+
+  getBoard() {
+    return {
+      lanes: [
+        this.generateLane('new_task', 'New', this.getStateEventName('new_task')),
+        this.generateLane('in_development', 'In Dev', this.getStateEventName('in_development')),
+        this.generateLane('in_qa', 'In QA', this.getStateEventName('in_qa',)),
+        this.generateLane('in_code_review', 'In CR', this.getStateEventName('in_code_review')),
+        this.generateLane('ready_for_release', 'Ready for release', this.getStateEventName('ready_for_release')),
+        this.generateLane('released', 'Released', this.getStateEventName('released',)),
+        this.generateLane('archived', 'Archived', this.getStateEventName('archived')),
+      ],
+    };
+  }
+
+  loadLines() {
+    this.loadLine('new_task');
+    this.loadLine('in_development');
+    this.loadLine('in_qa');
+    this.loadLine('in_code_review');
+    this.loadLine('ready_for_release');
+    this.loadLine('released');
+    this.loadLine('archived');
+  }
+
+  componentDidMount() {
+    this.loadLines();
+  }
+
+  loadLine(state, page = 1) {
+    this.fetchLine(state, page).then((data) => {
+      this.setState({
+        [state]: data,
+      });
+    });
+  }
+
+  fetchLine(state, page = 1) {
+    let params = {q: { state_eq: state }, page, per_page: 10, format: 'json'};
+    let tasks_url = window.Routes.api_v1_tasks_path(params);
+
+    return fetch('GET', tasks_url)
+      .then(({ data }) => data);
+  }
+
+  onLaneScroll = (requestedPage, state) => {
+    return this.fetchLine(state, requestedPage).then(({items}) => {
+      return items.map((task) => {
+        return {
           ...task,
           label: task.state,
-          title: task.name,
-        })) : [],
-      };
-    }
-
-    getBoard() {
-      return {
-        lanes: [
-          this.generateLane('new_task', 'New'),
-          this.generateLane('in_development', 'In Dev'),
-          this.generateLane('in_qa', 'In QA'),
-          this.generateLane('in_code_review', 'In CR'),
-          this.generateLane('ready_for_release', 'Ready for release'),
-          this.generateLane('released', 'Released'),
-          this.generateLane('archived', 'Archived'),
-        ],
-      };
-    }
-
-    loadLines() {
-      this.loadLine('new_task');
-      this.loadLine('in_development');
-      this.loadLine('in_qa');
-      this.loadLine('in_code_review');
-      this.loadLine('ready_for_release');
-      this.loadLine('released');
-      this.loadLine('archived');
-    }
-
-    componentDidMount() {
-      this.loadLines();
-    }
-
-    loadLine(state, page = 1) {
-      this.fetchLine(state, page).then((data) => {
-        this.setState({
-          [state]: data,
-        });
+          title: task.name
+        };
       });
-    }
+    })
+  }
 
-    fetchLine(state, page = 1) {
-      return fetch('GET', window.Routes.api_v1_tasks_path({
-        q: { state_eq: state }, page, per_page: 10, format: 'json',
-      })).then(({ data }) => data);
-    }
+  handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
+    let update_task_url = window.Routes.api_v1_task_path(cardId, { format: 'json' });
+    let body = { task: { state_event: this.getStateEventName(targetLaneId) } };
 
-    onLaneScroll = (requestedPage, state) => this.fetchLine(state, requestedPage).then(({ items }) => items.map(task => ({
-      ...task,
-      label: task.state,
-      title: task.name,
-    })))
+    fetch('PUT', update_task_url, body)
+      .then(() => {
+        this.loadLine(sourceLaneId);
+        this.loadLine(targetLaneId);
+      });
+  }
 
-    render() {
-      return (
-        <div>
-          <h1>Your tasks</h1>
-          <Board
-            onLaneScroll={this.onLaneScroll}
-            data={this.getBoard()}
-            customLaneHeader={<LaneHeader />}
-            cardsMeta={this.state}
-          />
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Your tasks</h1>
+        <Board
+          draggable
+          laneDraggable={false}
+          handleDragEnd={this.handleDragEnd}
+          onLaneScroll={this.onLaneScroll}
+          data={this.getBoard()}
+          customLaneHeader={<LaneHeader />}
+          cardsMeta={this.state}
+        />
+      </div>
+    );
+  }
 }
